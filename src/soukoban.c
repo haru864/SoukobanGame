0e11e7ffd82c6e9aa3bb0e7eb445f08b5bdc6825
haru864 <kawachigenji95@gmail.com>
2022-11-26 22:55:40 +0900

    moving player, can't proceed rock

--- src/soukoban.c
+++ src/soukoban.c
@@ -14,14 +14,44 @@ int main(int argc, char **argv)
 	char c, c2;
 
 	initscr();
-	// noecho();
-	echo();
+	noecho();
 	cbreak();
+	mvaddch(10, 20, '#');
+	print_player();
 
-	c = getch();
-	mvaddch(10, 10, c);
-	refresh();
-	c = getch();
+	while ((c = getch()) != 'q')
+	{
+		int new_y = y, new_x = x;
+		switch (c)
+		{
+		case 'w':
+			new_y--;
+			break;
+		case 's':
+			new_y++;
+			break;
+		case 'a':
+			new_x--;
+			break;
+		case 'd':
+			new_x++;
+			break;
+		default:
+			continue;
+		}
+
+		c2 = mvinch(new_y, new_x);
+		move(y, x);
+		if (c2 == '#')
+			continue;
+
+		mvaddch(y, x, ' ');
+		y = new_y;
+		x = new_x;
+
+		print_player();
+		refresh();
+	}
 
 	endwin();
 
